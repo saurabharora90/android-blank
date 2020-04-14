bd244c1dc794cff3f0dbe0639c33b040b9ee5ba7
Saurabh Arora <saurabharora90@gmail.com>
2020-04-15 01:37:31 +0800

    we shouldn't rely on the old state to decide if we need to show the loading states

--- app/src/main/java/com/saurabharora/android_blank/adapter/LoadingAdapter.kt
+++ app/src/main/java/com/saurabharora/android_blank/adapter/LoadingAdapter.kt
@@ -1,6 +1,5 @@
 package com.saurabharora.android_blank.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,8 +9,6 @@ import com.saurabharora.android_blank.paging.CityListEvent
 import kotlin.properties.Delegates
 import kotlin.reflect.KProperty
 
-private const val TAG = "LoadingAdapter"
-
 class LoadingAdapter(private val type: Type) : RecyclerView.Adapter<LoadingViewHolder>() {
 
     enum class Type {
@@ -20,20 +17,19 @@ class LoadingAdapter(private val type: Type) : RecyclerView.Adapter<LoadingViewH
 
     var state: CityListEvent by Delegates.observable(
         CityListEvent.Success.SuccessInitial,
-        { _: KProperty<*>, old: CityListEvent, new: CityListEvent ->
+        { _: KProperty<*>, _: CityListEvent, new: CityListEvent ->
             when (type) {
                 Type.Header -> {
-                    if (old is CityListEvent.Loading.LoadingPreviousPage && new is CityListEvent.Success.SuccessPreviousPage)
+                    if (new is CityListEvent.Success.SuccessPreviousPage)
                         notifyItemRemoved(0)
-                    else if (old is CityListEvent.Success.SuccessInitial && new is CityListEvent.Loading.LoadingPreviousPage)
+                    else if (new is CityListEvent.Loading.LoadingPreviousPage)
                         notifyItemInserted(0)
                 }
 
                 Type.Footer -> {
-                    Log.d(TAG, "observable() called with: old = $old, new = $new")
-                    if (old is CityListEvent.Loading.LoadingNextPage && new is CityListEvent.Success.SuccessNextPage)
+                    if (new is CityListEvent.Success.SuccessNextPage)
                         notifyItemRemoved(0)
-                    else if (old is CityListEvent.Success.SuccessPreviousPage && new is CityListEvent.Loading.LoadingNextPage)
+                    else if (new is CityListEvent.Loading.LoadingNextPage)
                         notifyItemInserted(0)
                 }
             }

725efb5df91ed22ab55b67e3847172bfa53ce488
Saurabh Arora <saurabharora90@gmail.com>
2020-06-25 21:01:32 +0800

    enable viewbinding

--- app/src/main/java/com/saurabharora/android_blank/MainActivity.kt
+++ app/src/main/java/com/saurabharora/android_blank/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.saurabharora.android_blank
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
